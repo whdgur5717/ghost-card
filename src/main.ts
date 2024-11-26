add7f1c97df9b447dd6ae7cd552f6afa172152ac
whdgur5717 <whdgur57117@gmail.com>
2024-11-26 21:45:05 +0900

    feat: implement card shuffle

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,19 @@
 import "./style.css"
+import { shuffle } from "./shuffle"
 
-document.querySelector<HTMLDivElement>("#app")!.innerHTML = `
-  <div>
-   
-    <h1>Vite + TypeScript</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite and TypeScript logos to learn more
-    </p>
-  </div>
-`
+const cards = ["1", "2", "3", "4", "5", "6"]
+
+document.querySelector("#card-container")!.innerHTML = cards
+  .map((card, i) => `<div class='card-item' style=order:${i}>${card}</div>`)
+  .join("")
+
+setInterval(() => {
+  const { swappedIndexes } = shuffle(cards)
+  const cardElements = Array.from(document.querySelectorAll<HTMLElement>(".card-item"))
+  const [i, j] = swappedIndexes
+
+  const orderI = cardElements[i].style.order
+  const orderJ = cardElements[j].style.order
+  cardElements[i].style.order = orderJ
+  cardElements[j].style.order = orderI
+}, 1000)
